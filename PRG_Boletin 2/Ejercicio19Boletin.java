6b228da20dfd801552e04603ebb5989e4370e6bb
hugomonntes <huugomontees@gmail.com>
2024-10-08 19:30:24 +0200

    Cambios de Prueba

--- PRG_Boletin 2/Ejercicio19Boletin.java
+++ PRG_Boletin 2/Ejercicio19Boletin.java
@@ -14,32 +14,31 @@ public class Ejercicio19Boletin {
         sc.nextLine();
         opcionD = sc.nextLine();
 
-        if(opcionD.equals("d")){
-            for(int i = 0; i <= numeroAstersicos; i++){
+        if (opcionD.equals("d")) {
+            for (int i = 0; i <= numeroAstersicos; i++) {
                 System.out.println(asterisco);
-                for(int contadorEspacios = 0; contadorEspacios <= i; contadorEspacios++){
+                for (int contadorEspacios = 0; contadorEspacios <= i; contadorEspacios++) {
                     System.out.print(space);
                 }
             }
         }
         if (opcionD.equals("i")) {
-            for(int i = 0; i < numeroAstersicos; i++){
-                for(int contadorEspacios = (numeroAstersicos - i); contadorEspacios > 0; contadorEspacios--){
+            for (int i = 0; i < numeroAstersicos; i++) {
+                for (int contadorEspacios = (numeroAstersicos - i); contadorEspacios > 0; contadorEspacios--) {
                     System.out.print(space);
                 }
-            System.out.println(asterisco);
+                System.out.println(asterisco);
+            }
         }
-    }                                                      
-    System.out.println("Prueba");
     }
 }
 
 /*
-19.a) Realizar un programa que dibuje una ristra de N asteriscos en pantalla (N es
+19.a) Realizar un programa que dibuje una ristra de N asteriscos en pantalla
+(N es
 un número introducido polo usuario).
 Ejemplo:
 Cuantos asteriscos deseas dibujar? 10
-**********
 b) Repetirlo pero dibujando la ristra en diagonal. El usuario escogerá se la
 dibuja de izquierda la derecha o de derecha la izquierda.
 */
