35b7f17e4fbdad98e343ad07ed6ba56e99cf0b8b
hugomonntes <huugomontees@gmail.com>
2024-10-09 20:17:54 +0200

    Edit For

--- PRG_Boletin 2 (Estructuras de Control)/Ejercicio20Boletin.java
+++ PRG_Boletin 2 (Estructuras de Control)/Ejercicio20Boletin.java
@@ -3,41 +3,18 @@ import java.util.Scanner;
 public class Ejercicio20Boletin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numeroAstersicos;
+        int numeroAsteriscos;
         String asterisco = "*";
         String space = " ";
-        String opcionD;
 
         System.out.print("Introduce el numero de asteriscos: ");
-        numeroAstersicos = sc.nextInt();
-        System.out.print("Introduce Derecha o Izquierda (D/I): ");
-        sc.nextLine();
-        opcionD = sc.nextLine();
+        numeroAsteriscos = sc.nextInt();
 
-        if (opcionD.equals("d")) {
-            for (int i = 0; i <= numeroAstersicos; i++) {
-                System.out.println(asterisco);
-                for (int contadorEspacios = 0; contadorEspacios <= i; contadorEspacios++) {
-                    System.out.print(space);
-                }
+        for (int i = 0; i <= numeroAsteriscos; i++) {
+            System.out.println(asterisco);
+            for (int z = 0; z <= numeroAsteriscos; z++) {
+                System.out.print(asterisco);
             }
         }
     }
 }
-
-/*
-20.a) Visualizar en pantalla una figura similar a la siguiente:
-*
-**
-***
-****
-*****
-siendo variable (decidido por el usuario) el número de líneas a mostrar.
-b) A continuación, y con la misma cantidad de líneas que en el caso anterior, dibuja
-una pirámide como esta:
-  *
- ***
- *****
- *******
-*********
-*/
