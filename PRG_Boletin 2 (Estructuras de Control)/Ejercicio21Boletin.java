3bbd0f8367dd2cfb901dd9bd806ec4a1cd9a861d
hugomonntes <huugomontees@gmail.com>
2024-10-10 22:01:05 +0200

    Dev Horse Game

--- PRG_Boletin 2 (Estructuras de Control)/Ejercicio21Boletin.java
+++ PRG_Boletin 2 (Estructuras de Control)/Ejercicio21Boletin.java
@@ -3,47 +3,134 @@ import java.util.Scanner;
 public class Ejercicio21Boletin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String SpiritDorsal = " :--:º ";
-        String SombraDorsal = " :--:º ";
-        String TornadoDorsal = "<";
-        String BecefaloDorsal = "º";
+        //#region variables inicio caballos + dibujos de los caballos. 
+        int posSpirit = 0;
+        int posSombra = 0;
+        int posTornado = 0;
+        int posBucefalo = 0;
+        int lineaDeMeta = 50;
+        String spiritDibujo = ":--:º";
+        String sombraDibujo = ":--:º";
+        String tornadoDibujo = ":--:º";
+        String bucefaloDibujo = ":--:º";
         String espaciosAvanzar = " ";
-        //TODO representar caballos mediante el dorsal y que vayan avanzado los caballos mediante una variable space que los empuje hasta la meta.
-        //TODO BORRAR LOS CABALLOS UNA VEZ PASA QUE SOLO AVANCE CON LOS ESPACIOS
-        int Tornado = 0;
-        int Bucefalo = 0;
-        int lineaDeMeta = 5;
+        String circuito = "================================================================";
+        //#endregion
+        //TODO Hacer que los dorsales y los nombres de los caballos salgan aleatoriamente en cada carrera
+        //TODO Hacer como un metodo de apostar por cada caballo
+        //TODO Que tengas un saldo inicial y si pierde dependiendo X posicion del caballo t reste hasta que llegues a importe 0
+        //TODO Hacer el IF para saber quien es el ganador
+        //TODO Sumar porcentaje a la victoria por ganar y sumar saldo al inicial
+        //TODO Restar saldos en caso de derrota del caballo
+
+        int saldoInicial = 500;
+        int saldoApostado;
+        int opcion;
+
+        do{
+            System.out.println("╔══════════════════════════════════════════════════════════╗");
+            System.out.println("║                 ¡Bienvenid@ al Hipódromo!                ║");
+            System.out.println("║                   Saldo Inicial de 500€                  ║");
+            System.out.println("║                     Elige tu caballo:                    ║");
+            System.out.println("╚══════════════════════════════════════════════════════════╝");
+
+            System.out.println("1. Spirit (Dorsal 13)");
+            System.out.println("2. Sombra (Dorsal 45)");
+            System.out.println("3. Tornado (Dorsal 98)");
+            System.out.println("4. Bucefalo (Dorsal 66)");
+            System.out.println("5. Salir del hipódromo.");
+            System.out.print("Selecciona tu caballo (1/4) o Pulsa (5) para Salir: ");
+            opcion = sc.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                System.out.println("Has seleccionado a Spirit ¡Suerte!");
+                System.out.println("Cantidad a apostar: ");
+                saldoApostado = sc.nextInt();
+                break;
+
+                case 2:
+                System.out.println("Has seleccionado a Sombra ¡Suerte!");
+                System.out.println("Cantidad a apostar: ");
+                saldoApostado = sc.nextInt();
+                break;
+
+                case 3:
+                System.out.println("Has seleccionado a Tornado ¡Suerte!");
+                System.out.println("Cantidad a apostar: ");
+                saldoApostado = sc.nextInt();
+                break;
+
+                case 4:
+                System.out.println("Has seleccionado a Bucefalo ¡Suerte!");
+                System.out.println("Cantidad a apostar: ");
+                saldoApostado = sc.nextInt();
+                break;
+
+                case 5: //FIXME Al introducir el 5 juega la carrera y no sale, sale dsps
+                System.out.println("Hasta luego te esperamos de vuelta!");
+                break;
+                
+                default:
+                System.out.println("Introduce una opción válida!");
+                break;
+            }
+
+            while (posSpirit < lineaDeMeta && posSombra < lineaDeMeta) {
+                for (int i = 0; i < 50; i++) {
+                    System.out.println();
+                }
+    
+                posSpirit += (int)(Math.ceil(Math.random() * 3));
+                posSombra += (int)(Math.ceil(Math.random() * 3));
+                posTornado += (int)(Math.ceil(Math.random() * 3));
+                posBucefalo += (int)(Math.ceil(Math.random() * 3));
+    
+                System.out.println(circuito);
+    
+                for (int i = 0; i < posSpirit; i++) {
+                    System.out.print(espaciosAvanzar);
+                }
+                System.out.println("(Spirit) " + spiritDibujo);
+    
+                for (int i = 0; i < posSombra; i++) {
+                    System.out.print(espaciosAvanzar);
+                }
+                System.out.println("(Sombra) " + sombraDibujo);
+    
+                for (int i = 0; i < posTornado; i++) {
+                    System.out.print(espaciosAvanzar);
+                }
+                System.out.println("(Tornado) " + tornadoDibujo);
+    
+                for (int i = 0; i < posBucefalo; i++) {
+                    System.out.print(espaciosAvanzar);
+                }
+                System.out.println("(Bucefalo) " + bucefaloDibujo);
+    
+                System.out.println(circuito);
+    
+                long startTime = System.currentTimeMillis();
+                while (System.currentTimeMillis() - startTime < 200) {
+                }
+            }
+
+        } while(opcion != 5);
 
         // System.out.println("--Carreras de Caballos--");
         // System.out.println("1. Spirit / Dorsal 70");
         // System.out.println("2. Sombra / Dorsal 45");
         // System.out.println("3. Tornado / Dorsal 13");
         // System.out.println("4. Bucéfalo / Dorsal 51");
-
-        for(int Spirit = 0; Spirit <= lineaDeMeta; Spirit += (int)(Math.ceil(Math.random() * 3))){
-            for(int x = 0; x <= Spirit; x++){
-                System.out.print(SpiritDorsal);
-            }
-            System.out.print(espaciosAvanzar);
-        }
-        System.out.print("\n");
-
-        for(int Sombra = 0; Sombra <= lineaDeMeta; Sombra += (int)(Math.ceil(Math.random() * 3))){
-            for(int x = 0; x <= Sombra; x++){
-                System.out.print(SombraDorsal);
-            }
-            System.out.print(espaciosAvanzar);
-        }
-        System.out.print("\n");
-
-        // for(int i = 0; Spirit < lineaDeMeta && Sombra < lineaDeMeta && Tornado < lineaDeMeta && Bucefalo < lineaDeMeta; i++){
-        //     Spirit += (int)(Math.ceil(Math.random() * 3));
-        //     for(int x = 0; x <= i; x++){
-        //         System.out.println(espaciosAvanzar);
-        //     }
-        //     Sombra += (int)(Math.ceil(Math.random() * 3));
-        //     Tornado += (int)(Math.ceil(Math.random() * 3));
-        //     Bucefalo += (int)(Math.ceil(Math.random() * 3));
+        // for(int i = 0; Spirit < lineaDeMeta && Sombra < lineaDeMeta && Tornado <
+        // lineaDeMeta && Bucefalo < lineaDeMeta; i++){
+        // Spirit += (int)(Math.ceil(Math.random() * 3));
+        // for(int x = 0; x <= i; x++){
+        // System.out.println(espaciosAvanzar);
+        // }
+        // Sombra += (int)(Math.ceil(Math.random() * 3));
+        // Tornado += (int)(Math.ceil(Math.random() * 3));
+        // Bucefalo += (int)(Math.ceil(Math.random() * 3));
         // }
 
         // System.out.printf("Spirit lleva %d /50 \n", Spirit);
@@ -53,12 +140,3 @@ public class Ejercicio21Boletin {
         // System.out.printf("\n");
     }
 }
-
-/*
- * 21.Carreras de “caballos”. Se pide al usuario que seleccione un caballo entre
- * el 1 y
- * el 4. Una vez seleccionado empieza la carrera: Aleatoriamente se selecciona
- * un
- * caballo entre 1 y 4 y se mueven entre una y 3 posiciones. Se debe ir viendo
- * como avanzan todos los caballos. Ver las notas del final.
- */
