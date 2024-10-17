5adc0a55d37402b1b547cc839e8322f97aeb1411
Hugo Montes <huugomontees@gmail.com>
2024-10-17 21:22:48 +0200

    Update

--- PRG_Boletin 3 (Métodos)/Ex6_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex6_b3.java
@@ -29,16 +29,19 @@ public class Ex6_b3 {
 }
 
 /*
-6. a) Realizar una función que halle y devuelva la potencia de un número ( No se
+6. 
+a) Realizar una función que halle y devuelva la potencia de un número ( No se
 permite usar funciones de Math). La base puede ser real y el exponente entero
 puede ser negativo (recuerda que a-b = (1/a)b
 ).
 Parámetros del método: la base y el exponente. Valor devuelto: la potencia
+
 b) Realiza un nuevo método (en la misma clase) que muestre en pantalla las n
 primeras potencias de un número a (n y a serán parámetros del método).
 Por ejemplo, si los parámetros son a=2 y n=4 mostrará: 1, 2, 4, 8.
 Para hacerlo deberás llamar al creado en el apartado anterior para calcular las
 potencias, no las puedes calcular de nuevo en este método.
+
 c) Escribe un método que sume la progresión geométrica (x puede ser real) y
 devuelva el resultado.
 1+x+x2+ x3+ x4+... xn
