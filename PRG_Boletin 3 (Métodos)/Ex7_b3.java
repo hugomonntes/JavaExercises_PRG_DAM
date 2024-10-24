b10c23cf9da12f1ecf9ce57bfc53ff8a7745bf0f
Hugo Montes <huugomontees@gmail.com>
2024-10-24 19:03:57 +0200

    Update

--- PRG_Boletin 3 (Métodos)/Ex7_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex7_b3.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class Ex7_b3 {
     public static String saveFile(String fileName){
         Scanner f = new Scanner(fileName);
-        String auxiliar = "";
+        String contentFile = "";
         while (f.hasNext()) {
-            auxiliar = f.nextLine();
-            System.out.println(auxiliar);
+            contentFile = f.nextLine();
+            System.out.println(contentFile);
         }
         f.close();
-        return auxiliar;
-    }
-
-    public static void appendFile(String fileName, String msg) {
-
+        return contentFile;
     }
 
     public static void main(String[] args) throws Exception {
@@ -25,18 +21,18 @@ public class Ex7_b3 {
 
 /*
  ** 7. a) Haz una función llamada saveFile a la cual se le pasa una cadena que
- * representa el nombre de un archivo. Dicha función debe leer un archivo
- * completo y
- * devolverlo como string.
- * Para probarla en el main usa el archivo del ejercicio 2.
- * b) Realiza una función denominada appendFile a la que se le pasan dos
- * cadenas, la
- * primera es el nombre de un archivo y la segunda es un texto que debe añadir
- * al
- * final del archivo. Para ello primero lee el archivo, concatena la nueva
- * cadenas y lo
- * guarda de nuevo. Utiliza la función creada en (a) para la lectura.
- * c) Realiza una función denominada appendFile2 que hace lo mismo que la
- * realizada
- * en (b) pero usando el método explicado en el Apéndice II de los apuntes.
+representa el nombre de un archivo. Dicha función debe leer un archivo
+completo y
+devolverlo como string.
+Para probarla en el main usa el archivo del ejercicio 2.
+b) Realiza una función denominada appendFile a la que se le pasan dos
+cadenas, la
+primera es el nombre de un archivo y la segunda es un texto que debe añadir
+al
+final del archivo. Para ello primero lee el archivo, concatena la nueva
+cadenas y lo
+guarda de nuevo. Utiliza la función creada en (a) para la lectura.
+c) Realiza una función denominada appendFile2 que hace lo mismo que la
+realizada
+en (b) pero usando el método explicado en el Apéndice II de los apuntes.
  */
