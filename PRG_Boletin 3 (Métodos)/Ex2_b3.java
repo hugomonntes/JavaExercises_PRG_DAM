80f7ce7a7e570dc41a7932aea798e99418108111
Hugo Montes <huugomontees@gmail.com>
2024-10-28 20:55:48 +0100

    Update

--- PRG_Boletin 3 (Métodos)/Ex2_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex2_b3.java
@@ -1,29 +1,33 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
+
 public class Ex2_b3 {
-    public static void lineasEnBlanco(int numeroLineas){//coment
-        for (int i = 0; i <= numeroLineas; i++){
+    public static void lineasEnBlanco(int numeroLineas) {// coment
+        for (int i = 0; i <= numeroLineas; i++) {
             System.out.println("");
         }
     }
 
     /**
      * Comprobar si es Par.
+     * 
      * @param number
      * @return true si es par, false si es impar.
      */
-    public static boolean isPar(int number){
-        return number % 2 == 0  ;
+    public static boolean isPar(int number) {
+        return number % 2 == 0;
     }
 
     /**
      * Comprobar si es Positivo.
+     * 
      * @param number2
      * @return "P" si es positivo, "N" si es negativo.
      */
-    public static String isPositive(int number2){ //char no string
+    public static String isPositive(int number2) { // char no string
         return number2 >= 0 ? "P" : "N";
     }
+
     public static void main(String[] args) throws Exception {
         String userName;
         int userNumber;
@@ -36,29 +40,24 @@ public class Ex2_b3 {
         System.out.println(isPar(userNumber));
         System.out.println(isPositive(userNumber));
 
-        //EX2
-        PrintWriter f = new PrintWriter("Ex2_b3.txt");
+        // EX2
+        PrintWriter f = new PrintWriter("PRG_Archivos/Ex2_b3.txt");
         f.printf("%s \n", userName);
         f.printf("%d \n", userNumber);
-        if (isPar(userNumber) && isPositive(userNumber).equals("P")) {//TODO reducir el código
-            f.printf("El número %d es par y positivo", userNumber);
-        } else if (!isPar(userNumber) && isPositive(userNumber).equals("P")){
-            f.printf("El número %d es impar y positivo", userNumber);
-        } else if (isPar(userNumber) && isPositive(userNumber).equals("N")){
-            f.printf("El número %d es par y negativo", userNumber);
-        } else{
-            f.printf("El número %d es impar y negativo", userNumber);
-        }
+        String isPar = isPar(userNumber) ? "par" : "impar";
+        String isPositive = isPositive(userNumber).equals("P") ? "positivo" : "negativo";
+        f.printf("El número %d es %s y %s", userNumber, isPar, isPositive);
 
         f.close();
     }
 }
 /*
-**2. Amplia el programa anterior (directamente el main) para que guarde en un
-archivo los siguientes datos: el nombre en una línea, el número en la siguiente y si
-es par/impar y positivo/negativo.
-El archivo queda algo así:
-Minerva McGonagall
--5
-El n.º es impar in negativo.
-*/
+ ** 2. Amplia el programa anterior (directamente el main) para que guarde en un
+ * archivo los siguientes datos: el nombre en una línea, el número en la
+ * siguiente y si
+ * es par/impar y positivo/negativo.
+ * El archivo queda algo así:
+ * Minerva McGonagall
+ * -5
+ * El n.º es impar in negativo.
+ */
--- PRG_Boletin 3 (Métodos)/Ex9_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex9_b3.java
@@ -1,45 +1,53 @@
+import java.io.File;
 import java.util.Scanner;
+
 public class Ex9_b3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         int number;
         int opcion;
         String fileUser;
         System.out.println("Introduce un número: ");
         number = sc.nextInt();
-        System.out.println(Ex8_b2.isPrimo(number)); //Print si es Primo o No.
-        Ex8_b2.mostrarPrimos(number); //Print los números primos hasta X número.
-        do{
-            System.out.printf("1.Mostrar numeros primos menores a %d", number);
-            System.out.printf("2.Primos en archivo");
-            System.out.printf("3.Archivo con primos");
-            System.out.printf("4.Salir");
+        do {
+            System.out.printf("1.Mostrar numeros primos menores a %d \n", number);
+            System.out.printf("2.Primos en archivo \n");
+            System.out.printf("3.Archivo con primos \n");
+            System.out.printf("4.Salir \n");
             opcion = sc.nextInt();
 
-            switch (opcion) {
-                case 1: Ex8_b2.mostrarPrimos(number);
-                break;
-                case 2: System.out.println("Introduce el nombre de un archivos");
-                        fileUser = sc.nextLine();
-                break;
-            
+            switch (opcion){
+                case 1:
+                    Ex8_b3.mostrarPrimos(number);
+                    break;
+                case 2:
+                    fileUser = "PRG_Archivos/Ex9_b3_Numeros.txt";
+                    Scanner freader = new Scanner(new File(fileUser));
+                    int contenido;
+                    while (freader.hasNext()) {
+                        contenido = freader.nextInt();
+                        Ex8_b3.isPrimo(contenido) ? System.out.println(contenido + "Es primo") : System.out.println(contenido + "No es primo");
+                    }
+                    break;
+
                 default:
                     break;
             }
-        } while(true);
+        } while (true);
     }
 }
 
 /*
-**9. Modifica el ejercicio anterior de los números primos (solo el main) de forma
-que hagas un menú clásico con estas opciones:
-1. Mostrar primos menores que uno dado: Esto es lo que ya estaba del
-ejercicio previo.
-2. Primos en archivo: Pide el nombre de un archivo que debe contener
-números, cada uno en una línea. Lee dicho archivo y muestra dichos números
-en pantalla diciendo si son o no primos.
-3. Archivo con primos: Similar a 1, se le pide un número mayor que 2 al
-usuario y guarda en un archivo todos los primos desde 2 hasta el que meta el
-usuario. Estarán todos en la misma línea separados por punto y coma (;).
-4 Salir: El programa solo finaliza si se selecciona esta opción. 
-*/
+ ** 9. Modifica el ejercicio anterior de los números primos (solo el main) de
+ * forma
+ * que hagas un menú clásico con estas opciones:
+ * 1. Mostrar primos menores que uno dado: Esto es lo que ya estaba del
+ * ejercicio previo.
+ * 2. Primos en archivo: Pide el nombre de un archivo que debe contener
+ * números, cada uno en una línea. Lee dicho archivo y muestra dichos números
+ * en pantalla diciendo si son o no primos.
+ * 3. Archivo con primos: Similar a 1, se le pide un número mayor que 2 al
+ * usuario y guarda en un archivo todos los primos desde 2 hasta el que meta el
+ * usuario. Estarán todos en la misma línea separados por punto y coma (;).
+ * 4 Salir: El programa solo finaliza si se selecciona esta opción.
+ */
