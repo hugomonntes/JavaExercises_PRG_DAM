aba354e6acbffcd226423a4e5b83d60ab17f4fcf
Hugo Montes <huugomontees@gmail.com>
2024-11-16 22:15:41 +0100

    Update

--- PRG_Boletin 3 (Métodos)/Ex4s_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex4s_b3.java
@@ -1,14 +1,29 @@
-public class piramide {
+public class Ex4s_b3 {
+    public static void bucles(){
+        for (int i = 40; i >= 5; i--) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        int acumulador = 1;
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                acumulador *= i;
+                System.out.println(acumulador);
+            }
+        }
+    }
     public static void mostrarPiramide(){
         for (int i = 10; i >= 1; i--) {
-            System.out.print(i + " ");
+            System.out.printf("%2d", i);
             for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j + " ");
+                System.out.printf("%2d", j);
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
         mostrarPiramide();
+        bucles();
     }
 }
