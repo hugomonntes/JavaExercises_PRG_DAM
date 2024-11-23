4ab76c2c91c95d8ff172f8857eea6aa950907385
Hugo Montes <huugomontees@gmail.com>
2024-11-23 10:44:33 +0100

    Update

--- PRG_Boletin 3 (Métodos)/Ex17_b3.java
+++ PRG_Boletin 3 (Métodos)/Ex17_b3.java
@@ -1,15 +1,23 @@
 public class Ex17_b3 {
-    public static void conversion(){
+    public static void conversion(int limit, boolean flag){
         String hex = "HEX";
         String dec = "DEC";
         String oct = "OCT";
-        System.out.printf(" %4s | %4s | %4s \n", dec, hex, oct);
-        System.out.println("----------------------");
-        for (int i = 0; i < 10; i++) {
-            System.out.printf(" %4d | %4h | %4o \n", i, i, i);
+        if (flag) {
+            System.out.printf(" %4s | %4s \n", dec, oct);
+            System.out.println("---------------");
+            for (int i = 0; i < 10; i++) {
+                System.out.printf(" %4d | %4o \n", i, i);
+            }
+        } else {
+            System.out.printf(" %4s | %4s \n", dec, hex);
+            System.out.println("---------------");
+            for (int i = 0; i < 10; i++) {
+                System.out.printf(" %4d | %4h \n", i, i);
+            }
         }
     }
     public static void main(String[] args) {
-        conversion();
+        conversion(100, true);
     }
 }
