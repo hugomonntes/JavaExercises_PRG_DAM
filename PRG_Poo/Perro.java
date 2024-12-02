9104c0439e94cde9a85d88b27ec99475cee12070
hugomonntes <huugomontees@gmail.com>
2024-12-02 11:51:29 +0100

    Update

---
+++ PRG_Poo/Perro.java
@@ -0,0 +1,13 @@
+public class Perro {
+    public String raza;
+    public String nombre;
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int a) {
+        edad = a;
+    }
+}
