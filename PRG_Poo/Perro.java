9196f94f04f8ab2f39ae3e6294a988bc06f58e86
hugomonntes <huugomontees@gmail.com>
2024-12-04 12:58:49 +0100

    Update

--- PRG_Poo/Perro.java
+++ PRG_Poo/Perro.java
@@ -15,3 +15,13 @@ public class Perro { // Planos a partir de ellos puedes construir objetos tipo P
         return fiebre > 39 ? true : false;
     }
 }
+
+// 1º Ejercicio repaso de GET y SET si un perro tiene fiebre o no mediante POO (Al parametro de un SET se le llama igual que a la propiedad EJEMPLO: setEdad(int edad))
+// 2º Utilizar la palabra reservada THIS (Se utiliza para diferenciar una variable o parametro que se llama igual que otra fuera del SCOPE, EJEMPLO:
+
+// private int edad;
+
+// public int getEdad(int edad) {
+//     this.edad = edad (El this.edad se refiere al edad del parámetro *NO DE LA VARIABLE DE ARRIBA*)
+//     return edad;
+// })
