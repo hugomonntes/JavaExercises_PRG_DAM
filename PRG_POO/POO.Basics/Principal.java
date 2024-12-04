c5e1d2497ba045bdbdd6e6271d46e74b79f4de36
hugomonntes <huugomontees@gmail.com>
2024-12-04 16:55:06 +0100

    Update

--- PRG_POO/POO.Basics/Principal.java
+++ PRG_POO/POO.Basics/Principal.java
@@ -1,5 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
-        
+        Pelota pelota = new Pelota();
+        pelota.tipo = "Baloncesto";
+        pelota.radio = 20;
+        System.out.printf("Tipo: %s, Radio: %d", pelota.tipo, pelota.radio);
     }
 }
