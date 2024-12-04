2be4855e17c2b673de4d1468f9efb6c300887707
hugomonntes <huugomontees@gmail.com>
2024-12-04 12:59:27 +0100

    Update

--- PRG_Poo/Principal.java
+++ PRG_Poo/Principal.java
@@ -1,7 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         Perro p = new Perro();
-        System.out.println(p);
         if (p.setTemperatura(40)) {
             System.out.println("Tiene fiebre");
         }
