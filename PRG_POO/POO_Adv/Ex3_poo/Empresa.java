224d867242edd01f73b24b178ef2596afbe569de
Hugo Montes <huugomontees@gmail.com>
2024-12-14 16:16:06 +0100

    Update

--- PRG_POO/POO_Adv/Ex3_poo/Empresa.java
+++ PRG_POO/POO_Adv/Ex3_poo/Empresa.java
@@ -4,4 +4,19 @@ public class Empresa {
     private int ganancias;
     private Directivo directivo;
     private Empleado empleados;
+
+    //#region SET & GET
+    public void setGanancias(int ganancias) {
+        this.ganancias = ganancias;
+    }
+
+    public int getGanancias() {
+        return ganancias;
+    }
+    //#endregion
+
+    //#region CONSTRUCTORES
+    public Empresa(Directivo directivo, Empleado empleados, Empleado empleados2){
+
+    }
 }
