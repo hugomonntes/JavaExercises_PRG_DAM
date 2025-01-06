227ce491a64c034882ce9da315da63b0b09f7398
Hugo Montes <huugomontees@gmail.com>
2025-01-06 12:26:29 +0100

    Update

--- PRG_POO/POO_Adv/Ex3_poo/Main.java
+++ PRG_POO/POO_Adv/Ex3_poo/Main.java
@@ -2,5 +2,9 @@ package PRG_POO.POO_Adv.Ex3_poo;
 
 public class Main {
     public static void main(String[] args) {
+        Directivo directivo = new Directivo();
+        Empleado empleado1 = new Empleado("Fran", "Martinez", 32, "123456789V", 1900.0);
+        Empleado empleado2 = new Empleado("Jaime", "Perez", 20, "987456321Z", 1300.0);
+        Empresa empresa = new Empresa(directivo, empleado1, 200000);
     }
 }
