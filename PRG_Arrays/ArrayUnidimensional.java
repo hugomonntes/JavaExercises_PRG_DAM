4549ed81d47c68d8a71164f3347f6d1ef539a5b9
Hugo Montes <huugomontees@gmail.com>
2025-01-08 12:59:11 +0100

    Update

---
+++ PRG_Arrays/ArrayUnidimensional.java
@@ -0,0 +1,10 @@
+package PRG_Arrays;
+
+public class ArrayUnidimensional {
+    public static void main(String[] args) {
+        double[] temperaturas = new double[4]; //Declaración de un Array y declaración longitud del Array
+        for (int i = 0; i < temperaturas.length; i++) {
+            System.out.println(temperaturas[i]);
+        }
+    }
+}
