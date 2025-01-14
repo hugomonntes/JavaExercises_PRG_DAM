be2921db79d508120facffc0e36a4ee9318a4f83
Hugo Montes <huugomontees@gmail.com>
2025-01-14 22:29:15 +0100

    Update

---
+++ PRG_Arrays/Arrays_Exercises/Ex2_Arrays.java
@@ -0,0 +1,24 @@
+package PRG_Arrays.Arrays_Exercises;
+
+public class Ex2_Arrays {
+    
+}
+
+/*
+2. Repite el ejercicio anterior pero adaptándolo a una matriz de tamaño n filas y m
+columnas y que guarde caracteres (char) aleatorios entre ‘A’ y ‘Z’. En el main
+pruébalo con una tabla de 3x4 y luego una de 4x3. Ten en cuenta que:
+• Para sacar un carácter aleatorio, debes sacar un numero aleatorio en el
+rango unicode de los caracteres deseados y castearlo a char.
+• En el apartado (b) debes mostrar el array con formato de tabla, además cada
+carácter de una fila debe tener una separación de al menos dos espacios con
+los de al lado. Debes mostrar también como cabecera el número de fila y de
+columna. Usa printf para la ordenación en columnas, no uses \t.
+Ejemplo:
+0 1 2 3
+0 D R A Y
+1 J O P C
+2 H P A S
+• El apartado (e) lógicamente tendrás que pasar 4 índices para intercambiar 2
+posiciones. 
+*/
