73202f0bc0e8d2b7b548bb4a5dafc2a7f8e161b8
hugomonntes <huugomontees@gmail.com>
2025-02-06 09:56:50 +0100

    Update

--- PRG_Arrays/Arrays_Exercises/Ex6_Arrays/Matriz.java
+++ PRG_Arrays/Arrays_Exercises/Ex6_Arrays/Matriz.java
@@ -8,22 +8,20 @@ public class Matriz {
         n = (int)(Math.random()*10);
         numerosEnteros = new int[n][n];
     }
-
     //MÉTODOS
     public static void mostrarMatriz(int[][] tabla){
-        for (int i = 0; i <= tabla.length; i++) {
-            System.out.printf("%3d", i);
-        }
-        System.out.println();
+        // for (int i = 0; i <= tabla.length; i++) {
+        //     System.out.printf("%4d", i);
+        // }
+        // System.out.println();
         for (int i = 0; i < tabla.length; i++) {
-            System.out.printf("%3d", i);
+            // System.out.printf("%4d", i);
             for (int j = 0; j < tabla[i].length; j++) {
-                System.out.printf("%3d", tabla[i][j]);
+                System.out.printf("%4d", tabla[i][j]);
             }
             System.out.println();
         } 
     }
-
     public int sumarElementosMatriz(){
         int acumulador = 0;
         for (int i = 0; i < numerosEnteros.length; i++) {
@@ -33,16 +31,29 @@ public class Matriz {
         }
         return acumulador;
     }
-
-    public int sumarElementosMatriz2(boolean flag){
-        for (int i = 0; i < numerosEnteros.length; i++) {
-            
+    public static void sumarElementosMatriz2(int[][] tabla,boolean flag){
+        for (int i = 0; i < tabla.length; i += tabla[i].length) {
+            System.out.println(tabla[i][i]);
         }
-        return 0;
     }
-
     public static void main(String[] args) {
         int[][] numerosEnteros = new int[5][5];
+        for (int i = 0; i < numerosEnteros.length; i++) {
+            for (int j = 0; j < numerosEnteros[i].length; j++) {
+                numerosEnteros[i][j] = (int)((Math.random() * 10) + 1);
+            }
+        }
         mostrarMatriz(numerosEnteros);
+        sumarElementosMatriz2(numerosEnteros, false);
     }
 }
+
+/*
+Un parámetro boolean: Si está a true devuelve la suma de los
+elementos de la diagonal principal de la matriz (la que va de 0,0 a
+n,n). Si está a false la suma del resto de los elementos (todos menos
+la diagonal)
+Trata de hacerlo con solo un bucle para la diagonal y ninguno para los
+que no son de la diagonal (si no te sale hazlo con varios bucles, se te
+dará pista en la validación).
+*/
