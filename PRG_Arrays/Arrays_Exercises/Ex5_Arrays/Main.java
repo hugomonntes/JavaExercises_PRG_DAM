5f34dfb8203c07df692889f048f53f36e8fd76e7
Hugo Montes <huugomontees@gmail.com>
2025-02-08 12:41:43 +0100

    Update

--- PRG_Arrays/Arrays_Exercises/Ex5_Arrays/Main.java
+++ PRG_Arrays/Arrays_Exercises/Ex5_Arrays/Main.java
@@ -1,6 +1,17 @@
 package PRG_Arrays.Arrays_Exercises.Ex5_Arrays;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Ventas ventas = new Ventas(2050);
+        ventas.mostrarGrafica();
+        Scanner freader = new Scanner(new File("Ventas.txt"));
+        File fwriter = new File("archivo.txt");
+        if (fwriter.exists()){
+
+        }
     }
 }
--- PRG_Arrays/Arrays_Exercises/Ex5_Arrays/Ventas.java
+++ PRG_Arrays/Arrays_Exercises/Ex5_Arrays/Ventas.java
@@ -1,5 +1,61 @@
 package PRG_Arrays.Arrays_Exercises.Ex5_Arrays;
 
+import java.time.LocalDate;
+
 public class Ventas {
+    public int[] ventasAnuales = new int[12]; //12 por los meses de año
+    private int año;
+
+    public void setAño(int año) {
+        if (año >= LocalDate.now().getYear()) {
+            this.año = LocalDate.now().getYear() - 1;
+        } else {
+            this.año = año;
+        }
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public Ventas(int año){
+        setAño(año);
+        for (int i = 0; i < ventasAnuales.length; i++) {
+            ventasAnuales[i] = (int)(Math.random()*1000);
+        }
+    }
     
+    public Ventas(int año, int[] ventasAnuales){
+        setAño(año);
+        if (this.ventasAnuales.length != 12) {
+            for (int i = 0; i < ventasAnuales.length; i++) {
+                ventasAnuales[i] = (int)(Math.random()*1000);
+            }
+        }
+        this.ventasAnuales = ventasAnuales;
+    }
+
+    //MÉTODOS
+    public int calcularMedia(){
+        int acumulador = 0;
+        for (int i = 0; i < ventasAnuales.length; i++) {
+            acumulador += ventasAnuales[i];
+        }
+        return acumulador / ventasAnuales.length;
+    }
+
+    public void mostrarGrafica(){
+        System.out.printf("Año %d:\n", getAño());
+        for (int i = 1; i <= ventasAnuales.length; i++) {
+            int calculoHastags = ventasAnuales[i - 1] / 100;
+            System.out.printf("Mes %2d (%3d):", i, ventasAnuales[i - 1]);
+            if (ventasAnuales[i - 1] < 100) {
+                System.out.print("#");
+            }
+            for (int j = 0; j < calculoHastags; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
 }
