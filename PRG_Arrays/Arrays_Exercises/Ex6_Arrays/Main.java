f5085cd2fda2c29ebcdce87af42e89f878597030
hugomonntes <huugomontees@gmail.com>
2025-02-10 13:57:55 +0100

    Update

--- PRG_Arrays/Arrays_Exercises/Ex6_Arrays/Main.java
+++ PRG_Arrays/Arrays_Exercises/Ex6_Arrays/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Matriz matriz = new Matriz(5);
         matriz.mostrarMatriz(matriz.numerosEnteros); //Mostrar matriz
         matriz.sumarElementosMatriz(); //Mostrar suma de todos los elementos
-        matriz.sumarElementosMatriz2(false); //Mostrar suma diagonal
-        matriz.sumarElementosMatriz3(2); //Mostrar suma de elementos de una fila
+        System.out.println(matriz.sumarElementosMatriz2(true)); //Mostrar suma diagonal
+        System.out.println(matriz.sumarElementosMatriz3(2)); //Mostrar suma de elementos de una fila
         matriz.numerosEnteros = matriz.borrarFila(matriz.numerosEnteros, 2);//Mostrar matriz sin una fila
         System.out.println();
         matriz.mostrarMatriz(matriz.numerosEnteros);//Mostrar matriz sin una fila
