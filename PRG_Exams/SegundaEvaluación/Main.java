7d8a63f33b33eada81ff8068a85d47d1e54829dd
Hugo Montes <huugomontees@gmail.com>
2025-02-15 14:12:47 +0100

    Update

--- PRG_Exams/SegundaEvaluación/Main.java
+++ PRG_Exams/SegundaEvaluación/Main.java
@@ -1,12 +1,35 @@
 package SegundaEvaluación;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Magos magos = new Magos();
         int[][] matrizMagos;
-        matrizMagos = magos.creaMatriz(10);
-        magos.muestraMatriz(matrizMagos);
-        System.out.println("Suma de poderes:"+ magos.poderDeMago(matrizMagos, 1));
-        magos.intercambioDePoder(matrizMagos, 1, 4);
+        int numeroMagoUsuario = 0;
+        boolean isChecked;
+        matrizMagos = magos.creaMatriz(11);
+        magos.muestraMatriz(matrizMagos); //Mostrar matriz
+        do {
+            try{
+                isChecked = true;
+                do {
+                    System.out.print("Introduce el mago que quieres: ");
+                    numeroMagoUsuario = sc.nextInt();
+                    if (numeroMagoUsuario < 0 || numeroMagoUsuario > matrizMagos.length) {
+                        isChecked = false;
+                    } else {
+                        isChecked = true;
+                    }
+                } while (!isChecked);
+                System.out.println("Poder: " + magos.poderDeMago(matrizMagos, numeroMagoUsuario));
+            } catch (Exception e){
+                System.out.println("Introduce un dato válido");
+                isChecked = false;
+                sc.nextLine();
+            }
+        } while(!isChecked);
     }
 }
+
