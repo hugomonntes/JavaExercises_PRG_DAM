14575666a440b1bc1096f9b59f9899ab4efc17fc
Hugo Montes <huugomontees@gmail.com>
2025-03-21 20:58:19 +0100

    Update

--- javaexercises/src/main/java/javaexercises/PRG_POO/POO_Adv/Ex1_poo/Main.java
+++ javaexercises/src/main/java/javaexercises/PRG_POO/POO_Adv/Ex1_poo/Main.java
@@ -30,18 +30,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double base;
-        double altura;
-        System.out.println("Introduce la base: ");
-        base = sc.nextInt();
-        System.out.println("Introduce la altura: ");
-        altura = sc.nextInt();
+        double base = 0;
+        double altura = 0;
+        boolean isChecked;
+        do {
+            isChecked = true;
+            try {
+                System.out.println("Introduce la base: ");
+                base = sc.nextInt();
+                System.out.println("Introduce la altura: ");
+                altura = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error! Introduce un numero válido");
+                isChecked = false;
+                sc.nextLine();
+            }
+        } while (!isChecked);
         Geometria rectangulo = new Geometria(true, 20, 13);
         Geometria triangulo = new Geometria();
         triangulo.setAltura(altura);
         triangulo.setBase(base);
-        System.out.printf("Perímetro: %.2f, Área: %.2f, Figura: %s, Base: %.2f, Altura: %.2f", rectangulo.perimetro(), rectangulo.area(), rectangulo.tipo(), rectangulo.getAltura(), rectangulo.getBase());
-        System.out.printf("\nPerímetro: %.2f, Área: %.2f, Figura: %s, Base: %.2f, Altura: %.2f, Diagonal: %.2f", triangulo.perimetro(), triangulo.area(), triangulo.tipo(), triangulo.getAltura(), triangulo.getBase(), triangulo.diagonal());
+        System.out.printf("Perímetro: %.2f, Área: %.2f, Figura: %s, Base: %.2f, Altura: %.2f", rectangulo.perimetro(),
+                rectangulo.area(), rectangulo.tipo(), rectangulo.getAltura(), rectangulo.getBase());
+        System.out.printf("\nPerímetro: %.2f, Área: %.2f, Figura: %s, Base: %.2f, Altura: %.2f, Diagonal: %.2f",
+                triangulo.perimetro(), triangulo.area(), triangulo.tipo(), triangulo.getAltura(), triangulo.getBase(),
+                triangulo.diagonal());
         sc.close();
     }
 }
