f8f151ccd30dfde65c4abbc5f2d6cc4383e70875
hugomonntes <huugomontees@gmail.com>
2025-03-24 12:23:27 +0100

    Update

--- javaexercises/src/main/java/javaexercises/PRG_Exceptions/Ex3.java
+++ javaexercises/src/main/java/javaexercises/PRG_Exceptions/Ex3.java
@@ -2,7 +2,11 @@ package javaexercises.PRG_Exceptions;
 
 public class Ex3 {
     public static void pedirDatoUser(String mensajeUser, int rangoDato){
+        try {
+
+        } catch (Exception e) {
         
+        }
     }
     public static void main(String[] args) {
         
@@ -13,9 +17,11 @@ public class Ex3 {
 3. Realiza una función para pedir un número entero al usuario en cierto rango de
 forma robusta. Los parámetros serán un mensaje previo a la petición del dato así
 como el rango que se usará para pedir el dato.
+
 Si el rango está mal (es decir debe haber al menos 2 valores entre el límite inferior
 y el superior), la cadena está vacía o es nula la función lanzará
 IllegalArgumentException (Usa throw).
+
 Si está todo correcto hará un bucle en el que pida un dato entero al usuario. Si ese
 dato no está en el rango o no es un número (compruébalo con try/catch) vuelve a
 pedir el dato informando del error al usuario.
