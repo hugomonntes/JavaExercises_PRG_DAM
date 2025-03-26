697b5ecd320db42ab5fe59fd8ca3da1191d16464
hugomonntes <huugomontees@gmail.com>
2025-03-26 12:54:51 +0100

    Update

--- javaexercises/src/main/java/javaexercises/PRG_Exceptions/Ex3.java
+++ javaexercises/src/main/java/javaexercises/PRG_Exceptions/Ex3.java
@@ -1,31 +1,44 @@
 package javaexercises.PRG_Exceptions;
 
-public class Ex3 {
-    public static void pedirDatoUser(String mensajeUser, int rangoDato){
-        try {
+import java.util.Scanner;
 
-        } catch (Exception e) {
-        
-        }
+public class Ex3 {
+    public static void pedirDatoUser(String mensajeUser, int rangoMínimo, int rangoMáximo) {
+
     }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un mensaje: ");
+        String mensajeUser = sc.nextLine();
+        System.out.println("Introduce el rango mínimo: ");
+        int rangoMínimo = sc.nextInt();
+        System.out.println("Introduce el rango máximo: ");
+        int rangoMáximo = sc.nextInt();
+        pedirDatoUser(mensajeUser, rangoMínimo, rangoMáximo);
+        sc.close();
     }
 }
 
 /*
-3. Realiza una función para pedir un número entero al usuario en cierto rango de
-forma robusta. Los parámetros serán un mensaje previo a la petición del dato así
-como el rango que se usará para pedir el dato.
-
-Si el rango está mal (es decir debe haber al menos 2 valores entre el límite inferior
-y el superior), la cadena está vacía o es nula la función lanzará
-IllegalArgumentException (Usa throw).
-
-Si está todo correcto hará un bucle en el que pida un dato entero al usuario. Si ese
-dato no está en el rango o no es un número (compruébalo con try/catch) vuelve a
-pedir el dato informando del error al usuario.
-Consejo: Crea una función similar para reales y mételas ambas en una librería
-instalada como proyecto maven. Te servirá par el resto de ejercicios. Fíjate que te
-hubiera valido para el ejercicio 1. 
-*/
+ * 3. Realiza una función para pedir un número entero al usuario en cierto rango
+ * de
+ * forma robusta. Los parámetros serán un mensaje previo a la petición del dato
+ * así
+ * como el rango que se usará para pedir el dato.
+ * 
+ * Si el rango está mal (es decir debe haber al menos 2 valores entre el límite
+ * inferior
+ * y el superior), la cadena está vacía o es nula la función lanzará
+ * IllegalArgumentException (Usa throw).
+ * 
+ * Si está todo correcto hará un bucle en el que pida un dato entero al usuario.
+ * Si ese
+ * dato no está en el rango o no es un número (compruébalo con try/catch) vuelve
+ * a
+ * pedir el dato informando del error al usuario.
+ * Consejo: Crea una función similar para reales y mételas ambas en una librería
+ * instalada como proyecto maven. Te servirá par el resto de ejercicios. Fíjate
+ * que te
+ * hubiera valido para el ejercicio 1.
+ */
