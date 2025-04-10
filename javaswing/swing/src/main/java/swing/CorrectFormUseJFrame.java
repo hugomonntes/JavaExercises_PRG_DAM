18d9ca7b14b0477bdc4ca3ff6c6597349d83b455
hugomonntes <huugomontees@gmail.com>
2025-04-10 12:40:29 +0200

    Update

--- javaswing/swing/src/main/java/swing/CorrectFormUseJFrame.java
+++ javaswing/swing/src/main/java/swing/CorrectFormUseJFrame.java
@@ -17,21 +17,24 @@ public class CorrectFormUseJFrame extends JFrame {
         // setLayout(new FlowLayout()); // Style Layout
         setLayout(null); // Style sin Layout Si no estableces tamaños no funciona
         JLabel etiqueta1 = new JLabel("Etiqueta Uno");
-        JLabel etiqueta2 = new JLabel("Etiqueta Dos");
-        JButton boton1 = new JButton("Aceptar");
-        JButton boton2 = new JButton("Cancelar");
         etiqueta1.setLocation(0, 0);
-        etiqueta2.setLocation(100, 0);
-        boton1.setLocation(0, 100);
-        boton2.setLocation(100, 100);
-        this.add(etiqueta1, BorderLayout.NORTH);
-        this.add(etiqueta2, BorderLayout.SOUTH);
-        this.add(boton1, BorderLayout.EAST);
-        this.add(boton2, BorderLayout.WEST);
-
         etiqueta1.setSize(100, 20);
+        etiqueta1.setBounds(0,0,100,20); // Equivalente a setLocation y setSize pero los dos juntos
+        this.add(etiqueta1, BorderLayout.NORTH);
+
+        JLabel etiqueta2 = new JLabel("Etiqueta Dos");
+        etiqueta2.setLocation(100, 0);
         etiqueta2.setSize(100, 20);
+        this.add(etiqueta2, BorderLayout.SOUTH);
+        
+        JButton boton1 = new JButton("Aceptar");
+        boton1.setLocation(0, 100);
         boton1.setSize(100, 20);
+        this.add(boton1, BorderLayout.EAST);
+
+        JButton boton2 = new JButton("Cancelar");
+        boton2.setLocation(100, 100);
         boton2.setSize(100, 20);
+        this.add(boton2, BorderLayout.WEST);
     }
 }
