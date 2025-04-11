d150cd6cfc4fe779347af0eea910cd0d92283a88
hugomonntes <huugomontees@gmail.com>
2025-04-11 09:55:47 +0200

    Update

---
+++ javaswing/swing/src/main/java/swing/CajasTexto.java
@@ -0,0 +1,45 @@
+package swing;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class CajasTexto extends JFrame implements ActionListener {
+    JLabel lblDni;
+    JLabel lblNombre;
+    JTextField txtDni;
+    JTextField txtNombre;
+    JTextField txtInfo;
+
+    public CajasTexto() {
+        super("Cajas de edición de texto");
+        this.setLayout(new FlowLayout());
+        lblDni = new JLabel("Introduzca su DNI");
+        add(lblDni);
+        txtDni = new JTextField(9);
+        txtDni.addActionListener(this);
+        add(txtDni);
+        txtNombre = new JTextField("Escriba aquí su nombre");
+        txtNombre.addActionListener(this);
+        add(txtNombre);
+        txtInfo = new JTextField("Información no editable", 23);
+        txtInfo.setEditable(false);
+        txtInfo.addActionListener(this);
+        add(txtInfo);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == txtDni || e.getSource() == txtInfo) {
+            ((JTextField) e.getSource()).selectAll();
+        }
+        JOptionPane.showMessageDialog(null,
+                ((JTextField) e.getSource()).getSelectedText(),
+                "Cajas de Texto", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
--- javaswing/swing/src/main/java/swing/GUI.java
+++ javaswing/swing/src/main/java/swing/GUI.java
@@ -6,7 +6,8 @@ public class GUI {
     public static void main(String[] args) {
         // CorrectFormUseJFrame window = new CorrectFormUseJFrame();
         // Eventos1 window = new Eventos1();
-        Eventos2 window = new Eventos2();
+        // Eventos2 window = new Eventos2();
+        CajasTexto window = new CajasTexto();
         window.setSize(300, 300);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
