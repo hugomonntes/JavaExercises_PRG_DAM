9d79da003be6cca469c58ca351f28647461b1a1a
Hugo Montes <huugomontees@gmail.com>
2025-04-18 13:29:21 +0200

    Update

--- javaexercises/src/main/java/javaexercises/PRG_PooAdv/PooAdv_B2/Ex4/Persona.java
+++ javaexercises/src/main/java/javaexercises/PRG_PooAdv/PooAdv_B2/Ex4/Persona.java
@@ -1,7 +1,42 @@
 package javaexercises.PRG_PooAdv.PooAdv_B2.Ex4;
 
 abstract class Persona {
-    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String dni;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDni() {
+        return dni;
+    }
 }
 
 // 4. Retoma del primer tema de POO las clases Empleado y Directivo y rehazlas
