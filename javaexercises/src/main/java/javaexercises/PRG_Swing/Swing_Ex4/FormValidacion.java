b4ad72f56183f58d241bef3c0309dfd30f569fb5
hugomonntes <huugomontees@gmail.com>
2025-04-30 13:21:46 +0200

    Update

--- javaexercises/src/main/java/javaexercises/PRG_Swing/Swing_Ex4/FormValidacion.java
+++ javaexercises/src/main/java/javaexercises/PRG_Swing/Swing_Ex4/FormValidacion.java
@@ -16,16 +16,30 @@ public class FormValidacion extends JFrame implements ActionListener{
     private JButton btnCargar;
 
     public FormValidacion(){
-        // Interfaz
-        this.setTitle("Formulario Colores");
-        this.setLayout(new FlowLayout());
-        this.setVisible(true);
-        this.setSize(500, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         // Añadir Nombre textField
-        txfNombre = new JTextField();
+        txfNombre = new JTextField(10);
         this.add(txfNombre);
+        txfNombre.setSize(50, 100);
+        
+        // Añadir Edad textField
+        txfEdad = new JTextField(10);
+        this.add(txfEdad);
+        txfEdad.setSize(50, 100);
+        
+        // Añadir Direccion textField
+        txfDir = new JTextField(10);
+        this.add(txfDir);
+        txfDir.setSize(50, 100);
+        
+        // Añadir Boton Guardar
+        btnGuardar = new JButton();
+        this.add(btnGuardar);
+        btnGuardar.setSize(50, 100);
+
+        // Añadir Boton Cargar
+        btnCargar = new JButton();
+        this.add(btnCargar);
+        btnCargar.setSize(50, 100);
     }
 
     @Override
@@ -36,7 +50,8 @@ public class FormValidacion extends JFrame implements ActionListener{
 
 // 4. Haz una aplicación (layout null) que tenga 3 textfield con sus etiquetas
 // correspondientes. Una para nombre (txfNombre), otra para edad (txfEdad) y una
-// tercera para dirección (txfDir). Habrá dos botones:
+// tercera para dirección (txfDir). Habrá dos botones: (ok)
+
 // Guardar (btnGuardar): Si los datos del los textfield son válidos (no hay cadenas
 // vacías y en edad hay un número positivo o 0) los guarda en un archivo. Si existe
 // algún tipo de problema se informará con JOptionPane.showDialog() y un icono
--- javaexercises/src/main/java/javaexercises/PRG_Swing/Swing_Ex4/GraphicUserInterface.java
+++ javaexercises/src/main/java/javaexercises/PRG_Swing/Swing_Ex4/GraphicUserInterface.java
@@ -1,7 +1,16 @@
 package javaexercises.PRG_Swing.Swing_Ex4;
 
+import javax.swing.JFrame;
+
 public class GraphicUserInterface {
     public static void main(String[] args) {
         FormValidacion formValidacion = new FormValidacion();
+
+        // Interfaz
+        formValidacion.setTitle("Formulario Validación");
+        formValidacion.setLayout(null);
+        formValidacion.setVisible(true);
+        formValidacion.setSize(500, 500);
+        formValidacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
