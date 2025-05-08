5805a467c66f0a021b7e2a4b54d5c3e16c4dc859
hugomonntes <huugomontees@gmail.com>
2025-05-08 13:42:21 +0200

    Update

--- javaswingadv/src/main/java/javaswingadv/GUI.java
+++ javaswingadv/src/main/java/javaswingadv/GUI.java
@@ -5,7 +5,8 @@ import javax.swing.JFrame;
 public class GUI {
     public static void main(String[] args) {
         // EventosMouse eventos = new EventosMouse();
-        EventosKeyBoard eventos = new EventosKeyBoard();
+        // EventosKeyBoard eventos = new EventosKeyBoard();
+        Principal eventos = new Principal();
         eventos.setSize(500, 500);
         eventos.setVisible(true);
         eventos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
