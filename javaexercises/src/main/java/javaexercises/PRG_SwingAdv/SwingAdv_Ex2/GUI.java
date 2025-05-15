961c5715bddae90bcc281aae69b5cdda5bcfb413
hugomonntes <huugomontees@gmail.com>
2025-05-15 13:56:18 +0200

    Update

---
+++ javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/GUI.java
@@ -0,0 +1,5 @@
+package javaexercises.PRG_SwingAdv.SwingAdv_Ex2;
+
+public class GUI {
+    
+}
---
+++ javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/TecladoMovil.java
@@ -0,0 +1,22 @@
+package javaexercises.PRG_SwingAdv.SwingAdv_Ex2;
+
+public class TecladoMovil {
+    
+
+    public TecladoMovil(){
+        
+    }
+}
+
+// 2. Realizar un formulario que simule el teclado de un teléfono móvil clásico.
+// • Debe tener al menos 12 botones con los números del 1 al 9 en las tres
+// primeras filas y en la última la almohadilla (#), el 0 y el asterisco (*). Se
+// inicializarán los 12 mediante un bucle (no habrá 12 variables JButton).
+// • Cuando el ratón pase por encima de los botones cambiará de color y si se
+// pulsan quedarán permanentemente con un tercer color.
+// • Además se dispondrá de un JTextfield donde se mostrarán los números
+// pulsados. Será no editable.
+// • Si se pulsa una tecla que corresponda a un número también funcionará.
+// • Finalmente se creará otro botón más de reset (este no es necesario que se
+// haga en el bucle) mediante el cual se borra el textfield y se restaura el color
+// de todos los botones.
