24137e5d5a05072e139641a3e843f9a9aea409c8
Hugo Montes <huugomontees@gmail.com>
2025-05-17 15:42:06 +0200

    Update

--- javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/GUI.java
+++ javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/GUI.java
@@ -1,5 +1,7 @@
 package javaexercises.PRG_SwingAdv.SwingAdv_Ex2;
 
 public class GUI {
-    
+    public static void main(String[] args) {
+        TecladoMovil movil = new TecladoMovil();
+    }
 }
--- javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/TecladoMovil.java
+++ javaexercises/src/main/java/javaexercises/PRG_SwingAdv/SwingAdv_Ex2/TecladoMovil.java
@@ -1,22 +1,61 @@
 package javaexercises.PRG_SwingAdv.SwingAdv_Ex2;
 
-public class TecladoMovil {
-    
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-    public TecladoMovil(){
-        
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class TecladoMovil extends JFrame {
+    private JButton btnDefault;
+
+    public TecladoMovil() {
+        // Props Window
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+        this.setSize(500, 500);
+
+        for (int i = 1; i <= 12; i++) {
+            if (i < 10) {
+                btnDefault = new JButton(String.format("%d", i));
+            } else if (i == 10) {
+                btnDefault = new JButton(String.format("0"));
+            } else if (i == 11) {
+                btnDefault = new JButton(String.format("#"));
+            } else if (i == 12) {
+                btnDefault = new JButton(String.format("*"));
+            }
+            this.add(btnDefault);
+            btnDefault.addMouseListener(new EventosMouse());
+            btnDefault.addMouseMotionListener(new EventosMouse());
+        }
+    }
+
+    public class EventosMouse extends MouseAdapter {
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            btnDefault.setBackground(Color.LIGHT_GRAY);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            btnDefault.setBackground(null);
+        }
     }
 }
 
-// 2. Realizar un formulario que simule el teclado de un teléfono móvil clásico.
 // • Debe tener al menos 12 botones con los números del 1 al 9 en las tres
-// primeras filas y en la última la almohadilla (#), el 0 y el asterisco (*). Se
-// inicializarán los 12 mediante un bucle (no habrá 12 variables JButton).
+// primeras filas y en la última la almohadilla (#), el 0 y el asterisco (*).
+
+// Se inicializarán los 12 mediante un bucle (no habrá 12 variables JButton).
 // • Cuando el ratón pase por encima de los botones cambiará de color y si se
 // pulsan quedarán permanentemente con un tercer color.
 // • Además se dispondrá de un JTextfield donde se mostrarán los números
 // pulsados. Será no editable.
 // • Si se pulsa una tecla que corresponda a un número también funcionará.
 // • Finalmente se creará otro botón más de reset (este no es necesario que se
-// haga en el bucle) mediante el cual se borra el textfield y se restaura el color
+// haga en el bucle) mediante el cual se borra el textfield y se restaura el
+// color
 // de todos los botones.
