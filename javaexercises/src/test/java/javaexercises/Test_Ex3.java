e2cc10aa481e9bfdb0af96dafa5124212c4d0ceb
hugomonntes <huugomontees@gmail.com>
2025-05-28 16:46:02 +0200

    Update

--- javaexercises/src/test/java/javaexercises/Test_Ex3.java
+++ javaexercises/src/test/java/javaexercises/Test_Ex3.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -57,16 +58,23 @@ public class Test_Ex3 {
         assertEquals(0, Ex1_Arrays.minimo(vIguales));
         assertEquals(-9999, Ex1_Arrays.minimo(vNegativos));
         assertEquals(0, Ex1_Arrays.minimo(vUno));
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+        try {
             Ex1_Arrays.minimo(vVacio);
-        });
-        assertThrows(NullPointerException.class, () -> {
+            fail("Fallo vector vacío");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            assertTrue(true);
+        }
+
+        try {
             Ex1_Arrays.minimo(null);
-        });
+            fail("Fallo vector nulo");
+        } catch (NullPointerException e) {
+            assertTrue(true);
+        }
     }
 
     @Test
-    public void testVectorIntercambio() {
+    public void testVectorIntercambio() { // TODO faltan pruebas
         assertTrue(Ex1_Arrays.intercambiarDatos(vOrdenado, 0, 2));
         assertArrayEquals(new int[] { 3, 2, 1, 4, 5 }, vOrdenado);
 
@@ -75,6 +83,11 @@ public class Test_Ex3 {
 
         assertFalse(Ex1_Arrays.intercambiarDatos(vPositivos, 0, 9));
         assertFalse(Ex1_Arrays.intercambiarDatos(vPositivos, -1, 2));
+        assertFalse(Ex1_Arrays.intercambiarDatos(vPositivos, 1, -1));
+        assertTrue(Ex1_Arrays.intercambiarDatos(vPositivos, 4, 3));
+
+        assertTrue(Ex1_Arrays.intercambiarDatos(vPositivos, 2, 2)); // Cambiar mismas posiciones
+        assertArrayEquals(vPositivos, vPositivos);
     }
 
     @Test
@@ -88,26 +101,38 @@ public class Test_Ex3 {
     }
 
     @Test
-    public void testSumarRangos(){
+    public void testSumarRangos(){ //TODO quitar assertThrows
         assertEquals(3, sumaRango(vPositivos, 0, 1));
         assertEquals(-1008, sumaRango(vNegativos, 1, vNegativos.length - 1));
         assertEquals(0, sumaRango(vIguales, 1, vNegativos.length - 1));
         assertEquals(0, sumaRango(vUno, 0, vNegativos.length - 1));
 
-        assertThrows(IllegalArgumentException.class, () -> { // Test v == null
+        try {
             sumaRango(null, 0, 2);
-        });
+            fail("Fallo vector null");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
 
-        assertThrows(IllegalArgumentException.class, () -> { // Test inicio > fin
+        try {
             sumaRango(vPositivos, 3, 2);
-        });
-
-        assertThrows(IllegalArgumentException.class, () -> { // Test fin > longitud
+            fail("Fallo inicio mayor a fin");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+        
+        try {
             sumaRango(vPositivos, 0, vPositivos.length);
-        });
+            fail("Fallo fin mayor a longitud");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
 
-        assertThrows(IllegalArgumentException.class, () -> { // Test tamaño 0
+        try {
             sumaRango(vVacio, 0, vPositivos.length);
-        });
+            fail("Fallo vector tamaño cero");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
     }
 }
